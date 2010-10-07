3e0793730115e061a0088a7f83bfa0ff3c819d42
Josip Medved <jmedved@jmedved.com>
2010-10-08 00:04:59 +0200

    Medo: SingleInstance: Added IsOtherInstanceRunning method.

--- Source/Medo-Test/Properties/AssemblyInfo.cs
+++ Source/Medo-Test/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.00.11573.10330")]
-[assembly: AssemblyFileVersion("1.00.11573.10330")]
+[assembly: AssemblyVersion("1.00.11575.22011")]
+[assembly: AssemblyFileVersion("1.00.11575.22011")]
