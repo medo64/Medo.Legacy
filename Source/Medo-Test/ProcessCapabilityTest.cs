0c60336019b2f3afcb66c1b8eeee1e35993f6567
Josip Medved <jmedved@jmedved.com>
2011-03-05 10:22:29 +0100

    Initial version of ProcessCapability.

---
+++ Source/Medo-Test/ProcessCapabilityTest.cs
@@ -0,0 +1,97 @@
+﻿using Medo.Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ProcessCapabilityTest and is intended
+    ///to contain all ProcessCapabilityTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ProcessCapabilityTest {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext {
+            get {
+                return testContextInstance;
+            }
+            set {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        [TestMethod()]
+        public void BasicMeanAndStDevTest1() {
+            ProcessCapability target = new ProcessCapability();
+            target.Add(0);
+            target.Add(0);
+            target.Add(14);
+            target.Add(14);
+            Assert.AreEqual(7, target.Mean);
+            Assert.AreEqual(7, target.StDev);
+        }
+
+        [TestMethod()]
+        public void BasicMeanAndStDevTest2() {
+            ProcessCapability target = new ProcessCapability();
+            target.Add(0);
+            target.Add(6);
+            target.Add(8);
+            target.Add(14);
+            Assert.AreEqual(7, target.Mean);
+            Assert.AreEqual(5, target.StDev);
+        }
+
+        [TestMethod()]
+        public void BasicMeanAndStDevTest3() {
+            ProcessCapability target = new ProcessCapability();
+            target.Add(6);
+            target.Add(6);
+            target.Add(8);
+            target.Add(8);
+            Assert.AreEqual(7, target.Mean);
+            Assert.AreEqual(1, target.StDev);
+        }
+
+    }
+}
---
+++ Source/Medo/Math/ProcessCapability [000].cs
@@ -0,0 +1,159 @@
+﻿//Josip Medved <jmedved@jmedved.com>  http://www.jmedved.com  http://blog.jmedved.com
+
+//2011-05-05: Initial version.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Medo.Math {
+
+    /// <summary>
+    /// Statistical calculations for process capability.
+    /// </summary>
+    public class ProcessCapability : ICollection<double> {
+
+        private readonly List<double> Items = new List<double>();
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        public ProcessCapability() {
+        }
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        /// <param name="lowerLimit">Lower specification limit.</param>
+        /// <param name="upperLimit">Upper specification limit.</param>
+        public ProcessCapability(double lowerLimit, double upperLimit) {
+            this.LowerLimit = lowerLimit;
+            this.UpperLimit = upperLimit;
+        }
+
+
+        /// <summary>
+        /// Gets lower specification limit.
+        /// </summary>
+        public double? LowerLimit { get; private set; }
+
+        /// <summary>
+        /// Gets upper specification limit.
+        /// </summary>
+        public double? UpperLimit { get; private set; }
+
+
+        /// <summary>
+        /// Returns mean value of all items of Double.NaN if there are no items.
+        /// </summary>
+        public double Mean {
+            get {
+                if (this.Items.Count > 0) {
+                    double sum = 0;
+                    for (int i = 0; i < this.Items.Count; i++) {
+                        sum += this.Items[i];
+                    }
+                    return sum / this.Items.Count;
+                } else {
+                    return double.NaN;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns standard deviation (σ) of all items of Double.NaN if there are no items.
+        /// </summary>
+        public double StDev {
+            get {
+                if (this.Items.Count > 0) {
+                    double mean = this.Mean;
+                    double diffSum = 0;
+                    for (int i = 0; i < this.Items.Count; i++) {
+                        diffSum += System.Math.Pow(this.Items[i] - mean, 2);
+                    }
+                    return System.Math.Sqrt((1.0 / this.Items.Count) * diffSum);
+                } else {
+                    return double.NaN;
+                }
+            }
+        }
+
+
+        #region IList
+
+        /// <summary>
+        /// Adds an item to the collection.
+        /// </summary>
+        /// <param name="item">The number to add.</param>
+        public void Add(double item) {
+            this.Items.Add(item);
+        }
+
+        /// <summary>
+        /// Removes all items from the collection.
+        /// </summary>
+        public void Clear() {
+            this.Items.Clear();
+        }
+
+        /// <summary>
+        /// Determines whether the collection contains a specific value.
+        /// </summary>
+        /// <param name="item">The number to locate in the collection.</param>
+        /// <returns>True if item is found in the collection; otherwise, false.</returns>
+        public bool Contains(double item) {
+            return this.Items.Contains(item);
+        }
+
+        /// <summary>
+        /// Copies the elements of the collection to an array, starting at a particular array index.
+        /// </summary>
+        /// <param name="array">The one-dimensional array that is the destination of the elements copied from collection. The array must have zero-based indexing.</param>
+        /// <param name="arrayIndex">The zero-based index in array at which copying begins.</param>
+        public void CopyTo(double[] array, int arrayIndex) {
+            this.Items.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        /// Gets the number of elements contained in the collection.
+        /// </summary>
+        public int Count {
+            get { return this.Items.Count; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the collection is read-only.
+        /// </summary>
+        public bool IsReadOnly {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Removes the first occurrence of a specific numver from the collection.
+        /// </summary>
+        /// <param name="item">The number to remove from the collection.</param>
+        /// <returns>True if item was successfully removed from the collection; otherwise, false. This method also returns false if item is not found in the original collection.</returns>
+        public bool Remove(double item) {
+            return this.Items.Remove(item);
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        public IEnumerator<double> GetEnumerator() {
+            return this.Items.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Exposes the enumerator, which supports a simple iteration over a non-generic collection.
+        /// </summary>
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            return this.Items.GetEnumerator();
+        }
+
+        #endregion
+
+    }
+}
