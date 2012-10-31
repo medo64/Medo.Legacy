08289e33308e76e01ebd141a0b6935f7de258d97
Josip Medved <jmedved@jmedved.com>
2012-10-30 23:16:04 -0700

    Added linear calibration.

---
+++ Source/Medo-Test/LinearCalibrationTest.cs
@@ -0,0 +1,147 @@
+﻿using Medo.Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test {
+
+    /// <summary>
+    ///This is a test class for LinearCalibrationTest and is intended
+    ///to contain all LinearCalibrationTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class LinearCalibrationTest {
+
+        public TestContext TestContext { get; set; }
+
+
+
+        [TestMethod()]
+        public void LinearCalibration_NoCalibration() {
+            LinearCalibration target = new LinearCalibration();
+            Assert.AreEqual(0, target.GetAdjustedValue(0));
+            Assert.AreEqual(1, target.GetAdjustedValue(1));
+            Assert.AreEqual(1, target.CorrelationCoefficient);
+        }
+
+
+        [TestMethod()]
+        public void LinearCalibration_OnePoint() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 1);
+            Assert.AreEqual(0, target.GetAdjustedValue(1));
+            Assert.AreEqual(1, target.CorrelationCoefficient);
+        }
+
+        [TestMethod()]
+        public void LinearCalibration_OnePoint_NoChange() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 0);
+            Assert.AreEqual(1, target.GetAdjustedValue(1));
+            Assert.AreEqual(1, target.CorrelationCoefficient);
+        }
+
+
+        [TestMethod()]
+        public void LinearCalibration_TwoPoint_Simple() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 1);
+            target.AddCalibrationPoint(100, 101);
+            Assert.AreEqual(50, target.GetAdjustedValue(51));
+            Assert.AreEqual(1, target.CorrelationCoefficient);
+        }
+
+        [TestMethod()]
+        public void LinearCalibration_TwoPoint_Double() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 0);
+            target.AddCalibrationPoint(100, 50);
+            Assert.AreEqual(50, target.GetAdjustedValue(25));
+            Assert.AreEqual(1, target.CorrelationCoefficient);
+        }
+
+
+        [TestMethod()]
+        public void LinearCalibration_ThreePoint_Simple() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 1);
+            target.AddCalibrationPoint(50, 51);
+            target.AddCalibrationPoint(100, 101);
+            Assert.AreEqual(25, target.GetAdjustedValue(26));
+            Assert.AreEqual(1, target.CorrelationCoefficient);
+        }
+
+        [TestMethod()]
+        public void LinearCalibration_ThreePoint_Double() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 0);
+            target.AddCalibrationPoint(50, 25);
+            target.AddCalibrationPoint(100, 50);
+            Assert.AreEqual(80, target.GetAdjustedValue(40));
+            Assert.AreEqual(1, target.CorrelationCoefficient);
+        }
+
+        [TestMethod()]
+        public void LinearCalibration_ThreePoint_Halve() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 0);
+            target.AddCalibrationPoint(50, 100);
+            target.AddCalibrationPoint(100, 200);
+            Assert.AreEqual(75, target.GetAdjustedValue(150));
+            Assert.AreEqual(1, target.CorrelationCoefficient);
+        }
+
+        [TestMethod()]
+        public void LinearCalibration_ThreePoint_Freestyle_1() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 0.5);
+            target.AddCalibrationPoint(50, 50);
+            target.AddCalibrationPoint(100, 99.5);
+            Assert.AreEqual(24.74747, System.Math.Round(target.GetAdjustedValue(25), 5));
+            Assert.AreEqual(50, target.GetAdjustedValue(50));
+            Assert.AreEqual(75.25253, System.Math.Round(target.GetAdjustedValue(75), 5));
+            Assert.AreEqual(1, target.CorrelationCoefficient);
+        }
+
+        [TestMethod()]
+        public void LinearCalibration_ThreePoint_Freestyle_2() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 1);
+            target.AddCalibrationPoint(50, 49);
+            target.AddCalibrationPoint(100, 100);
+            Assert.AreEqual(24.74747, System.Math.Round(target.GetAdjustedValue(25), 5));
+            Assert.AreEqual(50, target.GetAdjustedValue(50));
+            Assert.AreEqual(75.25253, System.Math.Round(target.GetAdjustedValue(75), 5));
+            Assert.AreEqual(0.99985, Math.Round(target.CorrelationCoefficient, 5));
+        }
+
+        [TestMethod()]
+        public void LinearCalibration_ThreePoint_Freestyle_3() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 1);
+            target.AddCalibrationPoint(20, -10);
+            target.AddCalibrationPoint(100, 50);
+            Assert.AreEqual(60.08439, System.Math.Round(target.GetAdjustedValue(25), 5));
+            Assert.AreEqual(104.38819, System.Math.Round(target.GetAdjustedValue(50), 5));
+            Assert.AreEqual(148.69198, System.Math.Round(target.GetAdjustedValue(75), 5));
+            Assert.AreEqual(0.93477, Math.Round(target.CorrelationCoefficient, 5));
+        }
+
+
+        [TestMethod()]
+        public void LinearCalibration_Multipoint() {
+            LinearCalibration target = new LinearCalibration();
+            target.AddCalibrationPoint(0, 0.025);
+            target.AddCalibrationPoint(1, 0.217);
+            target.AddCalibrationPoint(2, 0.388);
+            target.AddCalibrationPoint(3, 0.634);
+            target.AddCalibrationPoint(4, 0.777);
+            target.AddCalibrationPoint(5, 1.011);
+            target.AddCalibrationPoint(6, 1.166);
+            Assert.AreEqual(0.1929, Math.Round(target.Slope, 4));
+            Assert.AreEqual(0.0240, Math.Round(target.Intercept, 4));
+            Assert.AreEqual(0.9976, Math.Round(target.CoefficientOfDetermination, 4));
+            Assert.AreEqual(1.19, Math.Round(target.GetAdjustedValue(0.254), 2));
+        }
+
+    }
+}
---
+++ Source/Medo/Math/LinearCalibration [001].cs
@@ -0,0 +1,131 @@
+﻿//Copyright (c) 2012 Josip Medved <jmedved@jmedved.com>
+
+//2012-10-30: Initial version.
+
+
+using System;
+using System.Collections.Generic;
+
+namespace Medo.Math {
+
+    /// <summary>
+    /// Linear calibration using a least square regression.
+    /// </summary>
+    public class LinearCalibration {
+
+        private readonly List<KeyValuePair<double, double>> CalibrationPoints = new List<KeyValuePair<double, double>>();
+
+        /// <summary>
+        /// Creates new instance.
+        /// </summary>
+        public LinearCalibration() { }
+
+
+        /// <summary>
+        /// Adds new calibration point.
+        /// </summary>
+        /// <param name="knownValue">Reference value.</param>
+        /// <param name="measuredValue">Measured value.</param>
+        public void AddCalibrationPoint(double knownValue, double measuredValue) {
+            this.CalibrationPoints.Add(new KeyValuePair<double, double>(knownValue, measuredValue));
+            this.Ready = false;
+        }
+
+
+        private double _slope;
+        internal double Slope {
+            get {
+                if (this.Ready == false) { this.Prepare(); }
+                return this._slope;
+            }
+            private set { this._slope = value; }
+        }
+
+        private double _intercept;
+        internal double Intercept {
+            get {
+                if (this.Ready == false) { this.Prepare(); }
+                return this._intercept;
+            }
+            private set { this._intercept = value; }
+        }
+
+        private double _correlationCoefficient;
+        public double CorrelationCoefficient {
+            get {
+                if (this.Ready == false) { this.Prepare(); }
+                return this._correlationCoefficient;
+            }
+            private set { this._correlationCoefficient = value; }
+        }
+
+        private double _coefficientOfDetermination;
+        /// <summary>
+        /// Gets coefficient of determination (R^2) that indicates how well regression line fits calibration points.
+        /// </summary>
+        public double CoefficientOfDetermination {
+            get {
+                if (this.Ready == false) { this.Prepare(); }
+                return this._coefficientOfDetermination;
+            }
+            private set { this._coefficientOfDetermination = value; }
+        }
+
+
+        private bool Ready = false;
+
+        private void Prepare() {
+            if (this.CalibrationPoints.Count == 0) { //no calibration
+                this.Slope = 1;
+                this.Intercept = 0;
+                this.CorrelationCoefficient = 1;
+                this.CoefficientOfDetermination = 1;
+            } else if (this.CalibrationPoints.Count == 1) { //no calibration - just offset
+                this.Slope = 1;
+                this.Intercept = this.CalibrationPoints[0].Value - this.CalibrationPoints[0].Key;
+                this.CorrelationCoefficient = 1;
+                this.CoefficientOfDetermination = 1;
+            } else {
+
+                double n = this.CalibrationPoints.Count;
+                double sumX = 0;
+                double sumY = 0;
+                double sumX2 = 0;
+                double sumY2 = 0;
+                double sumXY = 0;
+                foreach (var point in this.CalibrationPoints) {
+                    var x = point.Key;
+                    var y = point.Value;
+                    sumX += x;
+                    sumY += y;
+                    sumX2 += x * x;
+                    sumY2 += y * y;
+                    sumXY += x * y;
+                }
+
+                var mT = (n * sumXY - sumX * sumY);
+                var mB = (n * sumX2 - sumX * sumX);
+                var m = mT / mB;
+                var r = mT / System.Math.Sqrt(mB * (n * sumY2 - sumY * sumY));
+
+                this.Slope = m;
+                this.Intercept = (sumY / n) - m * (sumX / n); ;
+                this.CorrelationCoefficient = r;
+                this.CoefficientOfDetermination = r * r;
+            }
+
+            this.Ready = true;
+        }
+
+
+        /// <summary>
+        /// Returns value adjusted using a least square regression.
+        /// </summary>
+        /// <param name="value">Value to adjust.</param>
+        public double GetAdjustedValue(double value) {
+            return (value - this.Intercept) / this.Slope;
+        }
+
+    }
+
+}
