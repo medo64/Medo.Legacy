2bcbd2022809522d559b70280b3d31777073d64c
Josip Medved <jmedved@jmedved.com>
2012-10-30 20:12:14 -0700

    Updated Croatian holidays.

---
+++ Source/Medo-Test/HolidayTest.cs
@@ -0,0 +1,53 @@
+﻿using Medo.Localization.Croatia;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+namespace Test {
+
+
+    /// <summary>
+    ///This is a test class for JmbgTest and is intended
+    ///to contain all JmbgTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class HolidayTest {
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod()]
+        public void HolidayTest_20020216() {
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 1, 1)));
+            Assert.AreEqual(false, Holiday.IsHoliday(new DateTime(2002, 1, 6)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 5, 1)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 6, 22)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 6, 25)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 8, 5)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 8, 15)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 10, 8)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 11, 1)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 12, 25)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 12, 26)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 3, 31)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 4, 1)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2002, 5, 30)));
+        }
+
+        [TestMethod()]
+        public void HolidayTest_2012() {
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 1, 1)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 1, 6)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 5, 1)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 6, 22)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 6, 25)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 8, 5)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 8, 15)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 10, 8)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 11, 1)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 12, 25)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 12, 26)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 4, 8)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 4, 9)));
+            Assert.AreEqual(true, Holiday.IsHoliday(new DateTime(2012, 6, 7)));
+        }
+
+    }
+}
